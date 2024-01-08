392492e386c454a0dab7d5c5f43f04316ab54183
Tadashi <70841724+Tran97@users.noreply.github.com>
2024-01-08 14:18:02 +0100

    first commit

---
+++ tests/test_data.py
@@ -0,0 +1,16 @@
+from mlops.data.make_dataset import mnist
+from tests import _PATH_DATA
+import os.path
+import pytest
+
+@pytest.mark.skipif(not os.path.exists(_PATH_DATA+"1"), reason="Data files not found")
+def test_data():
+    dataset = mnist(_PATH_DATA)
+    train_tensor, test_tensor = dataset
+    train_data = train_tensor[:][0]
+    test_data = test_tensor[:][0]
+    N_train = len(train_data)
+    N_test = len(test_data)
+    assert len(dataset[0]) == N_train, "Dataset did not have the correct number of samples"
+    assert len(dataset[1]) == N_test, "Dataset did not have the correct number of samples"
+    assert all([data_point.shape == (1,28,28) for data_point in train_data]), "Datapoints does not have the correct shape"
---
+++ tests/test_model.py
@@ -0,0 +1,8 @@
+from models.model import myawesomemodel
+import torch
+import pytest
+
+@pytest.mark.parametrize("test_input,expected", [(torch.randn(1,1,28,28), (1,10)), (torch.randn(2,1,28,28), (2,10))])
+def test_model(test_input, expected):
+    output = myawesomemodel(test_input)
+    assert output.shape == expected
